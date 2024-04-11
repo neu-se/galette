c85b4128bd11934eabb915ef659e083e359ecda3
katie <32645020+katherine-hough@users.noreply.github.com>
2024-04-11 16:41:54 -0400

    * Fixed mask for SecurityManager#getClassContext

--- galette-integration-tests/src/test/java/edu/neu/ccs/prl/galette/SecurityManagerITCase.java
+++ galette-integration-tests/src/test/java/edu/neu/ccs/prl/galette/SecurityManagerITCase.java
@@ -7,34 +7,58 @@ import org.junit.jupiter.api.Test;
 
 public class SecurityManagerITCase {
     @Test
-    void getClassContext() {
-        Class<?>[] context = C.getClassContext();
+    void getClassContextSuper() {
+        Class<?>[] context = C.getClassContextSuper();
+        List<Class<?>> expected =
+                Arrays.asList(TestSecurityManager.class, A.class, B.class, C.class, SecurityManagerITCase.class);
+        Assertions.assertEquals(expected, Arrays.asList(context).subList(0, expected.size()));
+    }
+
+    @Test
+    void getClassContextThis() {
+        Class<?>[] context = C.getClassContextThis();
         List<Class<?>> expected =
                 Arrays.asList(TestSecurityManager.class, A.class, B.class, C.class, SecurityManagerITCase.class);
         Assertions.assertEquals(expected, Arrays.asList(context).subList(0, expected.size()));
     }
 
     static class TestSecurityManager extends SecurityManager {
-        public Class<?>[] getClassContext() {
+        public Class<?>[] getClassContentSuper() {
             return super.getClassContext();
+        }
+
+        public Class<?>[] getClassContentThis() {
+            return this.getClassContext();
         }
     }
 
     static class A {
-        public static Class<?>[] getClassContext() {
-            return new TestSecurityManager().getClassContext();
+        public static Class<?>[] getClassContextSuper() {
+            return new TestSecurityManager().getClassContentSuper();
+        }
+
+        public static Class<?>[] getClassContextThis() {
+            return new TestSecurityManager().getClassContentThis();
         }
     }
 
     static class B {
-        public static Class<?>[] getClassContext() {
-            return A.getClassContext();
+        public static Class<?>[] getClassContextSuper() {
+            return A.getClassContextSuper();
+        }
+
+        public static Class<?>[] getClassContextThis() {
+            return A.getClassContextThis();
         }
     }
 
     static class C {
-        public static Class<?>[] getClassContext() {
-            return B.getClassContext();
+        public static Class<?>[] getClassContextSuper() {
+            return B.getClassContextSuper();
+        }
+
+        public static Class<?>[] getClassContextThis() {
+            return B.getClassContextThis();
         }
     }
 }
