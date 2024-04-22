5e24f7c9030842af0f4c8413ecab94e2234947fc
katie <32645020+katherine-hough@users.noreply.github.com>
2024-04-22 18:03:36 -0400

    * Started working on data analysis scripts

--- scripts/report_util.py
+++ scripts/report_util.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import scipy
+
+A12_BOUNDS = [0.56, 0.64, 0.71]
 
 
 def select(data, **kwargs):
@@ -13,6 +16,35 @@ def set_columns(data, **kwargs):
     return data
 
 
-def make_categorical(data, column):
-    data[column] = pd.Categorical(data[column], categories=list(data[column].unique()))
-    return data
+def a12(values1, values2):
+    """
+    Returns Vargha-Delaney A12 statistic.
+    Vargha, A., and Delaney, H. D. (2000).
+    A Critique and Improvement of the "CL" Common Language Effect Size Statistics of McGraw and Wong.
+    Journal of Educational and Behavioral Statistics, 25(2), 101–132.
+    https://doi.org/10.2307/1165329
+    """
+    a = scipy.stats.mannwhitneyu(values1, values2)[0] / (len(values1) * len(values2))
+    return 1 - a if a < 0.5 else a
+
+
+def mann_whitney(values1, values2):
+    return scipy.stats.mannwhitneyu(values1, values2, alternative='two-sided', use_continuity=True)[1]
+
+
+def compute_bucket(value, bounds):
+    for i, bound in enumerate(bounds):
+        if value < bound:
+            return i
+    return len(bounds)
+
+
+def compute_sig_level(n, alpha=0.05):
+    number_of_comparisons = 1 if n < 2 else n * (n - 1) / 2
+    return alpha / number_of_comparisons
+
+
+def compute_cartesian_index(data, columns):
+    categories = [data[c].unique() for c in columns]
+    # Create an index that it the cartesian product of all unique values in the specified columns
+    return pd.MultiIndex.from_product(categories, names=columns)
---
+++ scripts/trial.py
@@ -0,0 +1,72 @@
+import json
+import os
+
+from report_util import *
+
+DATA_FILE_NAME = 'report.csv'
+INFO_FILE_NAME = 'info.json'
+
+
+class Trial:
+    """Represents the results of a taint tracking tool on a benchmark or benchmark suite."""
+
+    def __init__(self, trial_dir):
+        self.data_file = os.path.join(trial_dir, DATA_FILE_NAME)
+        self.info_file = os.path.join(trial_dir, INFO_FILE_NAME)
+        self.valid = all(os.path.isfile(f) for f in [self.data_file, self.info_file])
+        self.info = {}
+        if os.path.isfile(self.info_file):
+            with open(self.info_file, 'r') as f:
+                self.info = json.load(f)
+        self.info['id'] = os.path.basename(trial_dir)
+
+    def get_data_frame(self):
+        data = pd.read_csv(self.data_file) \
+            .rename(columns=lambda x: x.strip())
+        return set_columns(data, **self.info)
+
+
+def find_trials(input_dir):
+    print(f'Searching for trials in {input_dir}.')
+    files = [os.path.join(input_dir, f) for f in os.listdir(input_dir)]
+    trials = list(map(Trial, filter(os.path.isdir, files)))
+    print(f"\tFound {len(trials)} trials.")
+    return trials
+
+
+def check_trials(trials):
+    print(f'Checking trials.')
+    result = []
+    for c in trials:
+        if not c.valid:
+            print(f"\tMissing required files for {c.info}.")
+        else:
+            result.append(c)
+    print(f'\t{len(result)} trials were valid.')
+    return result
+
+
+def combine_trials(trials, file):
+    print('Creating combined experiment CSV.')
+    data = pd.concat([t.get_data_frame() for t in trials]) \
+        .reset_index(drop=True)
+    print(f'\tWrote combined experiment CSV to {file}.')
+    return data
+
+
+def extract(input_dir, output_file):
+    trials = check_trials(find_trials(input_dir))
+    return combine_trials(trials, output_file)
+
+
+def assert_record_equality(frames):
+    if len(frames) <= 1:
+        return
+    record_sets = [set(list(map(tuple, frame.to_dict(orient='records')))) for frame in frames]
+    assert all(record_sets[0] == record_set for record_set in record_sets[1:])
+
+
+def assert_vendor_matches(counts):
+    vendors = list(counts['vendor'].unique())
+    for version in list(counts['version'].unique()):
+        assert_record_equality([select(counts, version=version, vendor=vendor) for vendor in vendors])
