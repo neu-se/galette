7770d5a7cc84e2ac9900a77d8dc16f7834e1ba53
katie <32645020+katherine-hough@users.noreply.github.com>
2024-04-24 14:43:14 -0400

    * Fixed functional script

--- scripts/functional.sh
+++ scripts/functional.sh
@@ -4,10 +4,9 @@ readonly VERSION=$2
 readonly TOOL=$3
 readonly PROJECT_ROOT=$(pwd)
 readonly EXPERIMENT_DIRECTORY="/experiment/katie/galette/"
-readonly SCRATCH_DIRECTORY="$(pwd)/scratch/"
+readonly RESOURCES_DIRECTORY="$(pwd)/resources/"
 readonly OUTPUT_DIRECTORY="$(pwd)/out/"
-readonly REPORT_FILE="$OUTPUT_DIRECTORY/report.csv"
-readonly INFO_FILE="$OUTPUT_DIRECTORY/info.json"
+readonly INFO_FILE="$RESULTS_DIRECTORY/info.json"
 readonly SETTINGS_FILE="$PROJECT_ROOT/settings.xml"
 
 # Write a trace for each command to standard error
@@ -24,6 +23,18 @@ mkdir -p "$RESULTS_DIRECTORY"
 # Create the output directory
 mkdir -p "$OUTPUT_DIRECTORY"
 
+# Create the resource directory
+mkdir -p "$RESOURCES_DIRECTORY"
+
+# Record configuration information
+echo "{
+  \"version\": \"$VERSION\",
+  \"tool\": \"$TOOL\",
+  \"commit_sha\": \"$(git --git-dir "$PROJECT_ROOT/.git" rev-parse HEAD)\",
+  \"branch_name\": \"$(git --git-dir "$PROJECT_ROOT/.git" rev-parse --abbrev-ref HEAD)\",
+  \"remote_origin_url\": \"$(git --git-dir "$PROJECT_ROOT/.git" config --get remote.origin.url)\"
+}" >"$INFO_FILE"
+
 # Copy Maven settings file
 cp "$EXPERIMENT_DIRECTORY/settings.xml" "$SETTINGS_FILE"
 
@@ -36,20 +47,12 @@ python3 -m pip install -r "$PROJECT_ROOT/scripts/requirements.txt"
 
 # Run the script
 python3 "$PROJECT_ROOT/scripts/functional.py" \
-  --report-file "$REPORT_FILE" \
-  --tool "$TOOL" \
-  --output-dir "$SCRATCH_DIRECTORY" \
+  --output-dir "$OUTPUT_DIRECTORY" \
   --version "$VERSION" \
+  --tool "$TOOL" \
+  --resources-dir "$RESOURCES_DIRECTORY" \
   --settings-file "$SETTINGS_FILE"
 
-# Record configuration information
-echo "{
-  \"version\": \"$VERSION\",
-  \"tool\": \"$TOOL\",
-  \"commit_sha\": \"$(git --git-dir "$PROJECT_ROOT/.git" rev-parse HEAD)\",
-  \"branch_name\": \"$(git --git-dir "$PROJECT_ROOT/.git" rev-parse --abbrev-ref HEAD)\",
-  \"remote_origin_url\": \"$(git --git-dir "$PROJECT_ROOT/.git" config --get remote.origin.url)\"
-}" >"$INFO_FILE"
 
 # Copy all normal files in the output directory to the results directory
 find "$OUTPUT_DIRECTORY" -maxdepth 1 -type f -exec cp -t "$RESULTS_DIRECTORY" {} +
